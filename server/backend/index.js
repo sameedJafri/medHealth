528e50eaf089a863f17d6f901c8efb762d2512f7
Sameed Jafri <sjafr066@uottawa.ca>
2024-09-28 01:03:34 -0400

    adding backend boiler

---
+++ server/backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const cors = require("cors");
+const env = require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
+
