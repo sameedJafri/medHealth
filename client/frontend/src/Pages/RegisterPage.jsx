154fbd52151534c1c192b9a4744358e809a1cc19
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-09-28 09:16:13 -0400

    login and register

--- client/frontend/src/Pages/RegisterPage.jsx
+++ client/frontend/src/Pages/RegisterPage.jsx
@@ -1,23 +1,44 @@
-import './RegisterPage.css';
+import "./RegisterPage.css";
 
+function RegisterPage() {
+  return (
+    <div>
+      <h1>
+        <span style={{ color: "green" }}>Med</span>
+        <span style={{ color: "#dddaa4" }}>Health</span>
+      </h1>
 
-function RegisterPage(){
-
-    return (
-        <div className="registration-form">
-          <form className="form">
-            <p className="title">Create an Account </p>
+      <div className="login-wrap">
+            <div className="card">
+                <form className="login-form">
+                    <p className="title">Creat an Account</p>
+                    <div className="flex">
+                        <input placeholder="First Name" type="text" />
+                        <input placeholder="Last Name" type="text" />
+                    </div> <br />
+           
+                    <div className="flex">
+                        <input placeholder="Age" type="number" />
+                        <input placeholder="Weight (kg)" type="number" />
+                    </div><br />
             
-            <div className="flex">
-              <input type = "text" />
-              <input type = "text" />
+                    <div className="flex">
+                        <input placeholder="Gender" type="text" />
+                        <input placeholder="height (cm)" type="number" />
+                    </div><br />
+            
+                    <input className="size" placeholder="Email" type="email" /><br />
+            
+                    <input className="size" placeholder="Password" type="password" /><br />
+            
+                    <div>
+                        <button className="submit size btn" type="submit"> Sign Up </button>
+                        <p className="signin"> Already have an account ? <a href="/">Sign in</a>{" "}</p>
+                    </div>
+                 </form>
             </div>
-            <input type = "email" />
-            <input type="password"/>
-            <button className="submit" type='submit'>Sign Up</button>
-            <p className="signin">Already have an account ? <a href="/">Sign in</a> </p>
-          </form>
         </div>
-      );
+    </div>
+  );
 }
 export default RegisterPage;
