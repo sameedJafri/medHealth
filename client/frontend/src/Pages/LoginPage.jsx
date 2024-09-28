79e99eda4571045de9a21173b440bd0041da12dc
Jaden Wright-Maurais <jadenwrightmaurais@gmail.com>
2024-09-28 07:33:07 -0400

    This is the finished login page and the semi-fixed registration page

--- client/frontend/src/Pages/LoginPage.jsx
+++ client/frontend/src/Pages/LoginPage.jsx
@@ -1,24 +1,29 @@
 //import React, { useState } from "react";
 import './LoginPage.css';
 
+
 function LoginPage() {
 
     return (
+        <div>
+            <h1><span style={{color:"green"}}>Med</span><span style={{color:"#dddaa4"}}>Health</span></h1>
         <div className="login-wrap">
             <div className="card">
                 <form className="login-form" >
                     <p className="title">Login</p>
                     <div className="userinfo">
-                        <input type="email" />
+                        <input placeholder="Email" type="email" />
                     </div>
                     <div className="userinfo">
-                        <input type= "password" />
+                        <input placeholder="Password" type= "password" />
                     </div>
-                    <input type="submit" value="Login" className="btn" />
-                    <p className="login">Don't have an account? <a href="/RegisterPage">Create Account</a></p>
+                    <input type="submit" value="LogIn" className="btn" /><br/>
+                    <input type="submit" value="SignUp" className="btn" />
+                    <p className="login">Forgot Your Password? <a href="/RegisterPage">Retrive Password</a></p>
                 </form>
             </div>
-            
+
+        </div>
         </div>
     );
 }
