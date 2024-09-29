9eb32e55f8a0b32d2bcd4e6cb3fd2e6c70686695
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-09-28 20:56:01 -0400

    THe welcome page

--- client/frontend/src/Pages/Welcome.jsx
+++ client/frontend/src/Pages/Welcome.jsx
@@ -1,28 +1,41 @@
 //import React, { useState } from "react";
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Welcome.css';
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Welcome.css";
 
 function LoginPage() {
-
     const navigate = useNavigate(); // Declare useNavigate at the top level of the component
 
     const handleRegisterClick = (event) => {
         event.preventDefault(); // Prevent default form submission behavior
         // Now use the navigate function
-        navigate('/RegisterPage'); // Redirect to the Register page
+        navigate("/RegisterPage"); // Redirect to the Register page
     };
 
-        return (
-            
+    return (
+        <div style={{ marginBottom: "50px",marginTop: "50px", marginLeft: "250px", width: "1000px",textAlign: "center"}}>
+            <h3 style={{ marginBottom: "50px" }}>Welcome to</h3>
+            <h1 style={{ marginBottom: "50px" }}>
+                <span style={{ color: "green" }}>Med</span>
+                <span style={{ color: "#dddaa4" }}>Health</span>
+            </h1>
             <div>
-                Welcome page<br/>
-                <a href="/RegisterPage">Register</a><br/>
-                <a href= "/LoginPage">Login</a>
-                
+
+            <div style={{ marginBottom: "50px" }}><p >MedHealth's misson is to help indivial to improve themselves by
+                        allowing them to track their health status. This web app is built to
+                        moniter the mental health status of an individual and organise the
+                        intake of their prescription. Singn up to explore the diffrent
+                        feartures offered by MedHealth.{" "}</p></div>
+                        
+                <a href="/RegisterPage">
+                    <input style={{ marginRigth: "20px" }} type="submit" value="SignUp" className="btn_"/>
+                </a>
+                <a href="/LoginPage">
+                    <input style={{ marginLeft: "20px" }} type="submit" value="LogIn" className="btn_"/>
+                </a>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
 export default LoginPage;
